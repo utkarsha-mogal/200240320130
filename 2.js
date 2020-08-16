ab49d806c630452d4dc10c3cc2f83c5308782b8b
utkarsha-mogal <66870237+utkarsha-mogal@users.noreply.github.com>
2020-08-16 16:26:14 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,25 @@
+
+<script>
+    function fun()
+    {
+        document.getElementById("username").valve = ";"
+    document.getElementById("password").valve=";"
+
+    }
+</script>
+
+    <form name="myform" onsubmit="return validateForm() method=" post" required;" >
+        <label>
+            <for =" username"> Username:</label><br>
+            <input type="username" id=" username" name="username"><br></br>
+
+                <label>
+
+                    <for ="password"> password:</label><br>
+                    <input type="password" id=" password" name="password"><br></br>
+                        <input onclick="fun()" type="submit" valve="submit">
+          </form>
+      </label>
+      </html>
+
+
